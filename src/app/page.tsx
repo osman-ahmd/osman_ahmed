da8c5ace3df6588d7363cb9462fe2f3c0e820951
Osman Ahmed <onahmed@uio.no>
2023-12-02 08:19:47 +0100

    added some information about myself

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,51 @@ export default function Home() {
   return (
     <div>
       <h1>Osman Ahmed</h1>
+
+      {/* TODO: Legg til jobb attest pdf-vedlegg */}
+      <div>
+        <h3>Arbeidserfaring</h3>
+
+        <div>
+          Kjell & Company
+          Selger...
+        </div>
+
+        <div>
+          Kjell & Company
+          Selger...
+        </div>
+
+        <div>
+          Foodora
+          
+        </div>
+      </div>
+
+      <div>
+        <h3>Utdanning</h3>
+
+        <div>
+          <h4>Ullern VGS</h4>
+        </div>
+
+        <div>
+          <h4>Universitet i Oslo</h4>
+        </div>
+      </div>
+
+      <div>
+        <h3>Egne prosjekter</h3>
+
+        <div>
+          Weather app
+        </div>
+
+        <div>
+          Al...
+        </div>
+      </div>
+
     </div>
   )
 }
